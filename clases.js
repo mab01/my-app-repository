f2fbcfbcc3cfaa563843dd5fae46e18fe4b0db31
mab_01 <baigorriamonica@gmail.com>
2021-06-19 15:49:14 -0300

    creacion clase User

---
+++ clases.js
@@ -0,0 +1,11 @@
+class User{
+    constructor(nickName, name, lastName, email,adress, password){
+        this.nickName=nickName;
+        this.name=name;
+        this.lastName=lastName;
+        this.email=email;
+        this.adress=adress;
+        this.password=password;
+        
+    }
+}
